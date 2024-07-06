03b508a7a3388edf7696526acaefba21fb7b9e1b
Your Name <your.email@example.com>
2024-07-06 09:37:27 -0400

    updates 06062024

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import HeaderBox from "@/components/HeaderBox"
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+
+    const loggedIn = { firstName: 'Nikita', lastName: 'Kasansky', email: 'nik**93@hotmail.com' };
+
     return (
-        <div>Home</div>
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+
+                RECENT TRANSACTIONS
+            </div>
+
+            <RightSideBar 
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 125.50 }, { currentBalance: 5.50 }]}
+            />
+        </section>
     )
-}
+} 
 
 export default Home
