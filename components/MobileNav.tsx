a2a3bc4028696b499ab55f4a0d658a92d27d2237
Your Name <your.email@example.com>
2024-07-10 12:08:40 -0400

    updates 20240710

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,6 +15,7 @@ import {
   import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import Footer from "./Footer";
   
 
 const MobileNav = ({ user }: MobileNavProps ) => {
@@ -72,7 +73,7 @@ const MobileNav = ({ user }: MobileNavProps ) => {
                             USER
                         </nav>
                     </SheetClose>
-                    FOOTER
+                    <Footer user={user} type="mobile"/>
                 </div>
 
 
